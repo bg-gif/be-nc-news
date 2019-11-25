189085ee4a8b8304016d8797e3d3e5b6a82cc4b6
bg-gif <goff.ben@gmail.com>
2019-11-25 13:56:01 +0000

    cors added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routers/api-router");
 // const loginRouter = require('./routers/login-router');
@@ -11,6 +12,7 @@ const {
   handle422s
 } = require("./errors");
 
+app.use(cors());
 app.use(express.json());
 // app.use('/login', loginRouter);
 // app.use(verify);
