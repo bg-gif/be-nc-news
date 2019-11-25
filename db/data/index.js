c8269bfa4e290154163f9b3099e71d37249f2430
bg-gif <goff.ben@gmail.com>
2019-11-25 09:22:49 +0000

    heroku update3

--- db/data/index.js
+++ db/data/index.js
@@ -3,6 +3,6 @@ const ENV = process.env.NODE_ENV || "development";
 const devData = require("./development-data");
 const testData = require("./test-data");
 
-const data = { development: devData, test: testData, production: devData };
+const data = { development: devData, test: testData, production: development };
 
 module.exports = data[ENV];
