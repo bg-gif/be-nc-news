e78fea77881c74e2afee28ddcb926243ccbd6ba7
bg-gif <goff.ben@gmail.com>
2019-11-27 12:50:34 +0000

    cascade comment on article delete again

--- db/migrations/20191104120558_create_table_articles.js
+++ db/migrations/20191104120558_create_table_articles.js
@@ -1,8 +1,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable("articles", table => {
     table.increments("article_id");
-    table.string("title").notNullable;
-    table.text("body").notNullable;
+    table.string("title").notNullable();
+    table.text("body").notNullable();
     table.integer("votes").defaultTo(0);
     table
       .string("topic")
@@ -13,7 +13,8 @@ exports.up = function(knex) {
       .string("author")
       .references("username")
       .inTable("users")
-      .onDelete("CASCADE").notNullable;
+      .onDelete("CASCADE")
+      .notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
   });
 };
